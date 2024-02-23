10c81804ff77c85db5604ed519c03c0dbedbbe5c
DESKTOP-5670IME\chii8 <chii81020@gmail.com>
2024-02-23 20:47:32 +0900

    indes.js追加 タイトルに動きをつけていい感じにした

---
+++ js/index.js
@@ -0,0 +1,65 @@
+// function([string1, string2],target id,[color1,color2])
+consoleText(["Craft++", "create and craft team"], "title", [
+    "rgb(216, 237, 255)",
+]);
+
+function consoleText(words, id, colors) {
+    if (colors === undefined) colors = ["#fff"];
+    let letterCount = 1;
+    let x = 1;
+    let waiting = false;
+    let target = document.getElementById(id);
+
+    // target.innerHTML = words[0].substring(0, letterCount);
+
+    target.setAttribute("style", "color:" + colors[0]);
+
+    setTimeout(() => {
+        const timerId = setInterval(() => {
+            if (letterCount === 0 && waiting === false) {
+                //テキスト挿入開始時
+
+                waiting = true;
+                target.innerHTML = words[0].substring(0, letterCount);
+
+                setTimeout(() => {
+                    let usedColor = colors.shift();
+                    colors.push(usedColor);
+                    let usedWord = words.shift();
+                    words.push(usedWord);
+                    x = 1;
+                    target.setAttribute("style", "color:" + colors[0]);
+
+                    waiting = false;
+
+                    if (usedWord === "Craft++") {
+                        // create and craft teamの文字列はフォントサイズ変更
+                        target.setAttribute("style", "font-size: 45px");
+                    } else {
+                        //停止
+                        clearInterval(timerId);
+                    }
+
+                    letterCount += x;
+                }, 1500);
+            } else if (
+                //テキスト挿入終了後
+
+                letterCount === words[0].length + 1 &&
+                waiting === false
+            ) {
+                waiting = true;
+                setTimeout(() => {
+                    x = -1;
+                    letterCount += x;
+                    waiting = false;
+                }, 2000);
+            } else if (waiting === false) {
+                //テキストの挿入と消去
+
+                target.innerHTML = words[0].substring(0, letterCount);
+                letterCount += x;
+            }
+        }, 95);
+    }, 500);
+}
